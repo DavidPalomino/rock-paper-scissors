23d40ad224fefc24a278f5161deb4f042f42369b
David Palomino <david.leandro.palomino.rivera@gmail.com>
2023-05-22 15:18:16 -0500

    Add Score Table Funtionality

--- logic.js
+++ logic.js
@@ -1,42 +1,59 @@
-function game(election) {
-    let selection = 0
-    let counter = 0
-    let autoSelection = ''
-    let randomNum = Math.floor(Math.random() * ( 3 - 1 + 1) + 1) 
+function startGame(playerSelection) {
+    let machineSelection = createRandomSelection()
+    let playInformation = play(machineSelection, playerSelection)
+    let result = playInformation[0]
+    alert(result)
+    setScoreTable(playInformation)
+}
 
-    //Rock Paper Scissors Choses
-    if(randomNum === 1){
-        autoSelection = 'rock'
-    }
-    if(randomNum === 2){
-        autoSelection = 'paper'
-    }
-    if(randomNum === 3){
-        autoSelection = 'scissors'
-    }
+function play(machineSelection, playerSelection) {
+    let playInformation = []
 
-    //Game
-        //Draw
-    if (election === autoSelection) {
-        alert("Draw")
+    if (playerSelection === machineSelection) {
+        result = 'Draw'
     }
         //Rock Win Condition
-    else if (autoSelection === 'rock' && election === 'scissors') {
-        alert('You Win')
+    else if (playerSelection === 'rock' && machineSelection === 'scissors') {
+        result = 'You Win'
+    }
+        //Paper Win Condition
+    else if (playerSelection === 'paper' && machineSelection === 'rock') {
+       result = 'You Win'
+    }
+        //Scissors Win Condition
+    else if (playerSelection === 'scissors' && machineSelection === 'paper') {
+        result = 'You Win'
+    }
+    else{
+        result = 'You Lose'
     }
 
-        //Paper Win Condition
-    else if (autoSelection === 'paper' && election === 'rock') {
-       alert('You Win')
+    playInformation.push(result,playerSelection, machineSelection)
+    return playInformation
+}
+
+function createRandomSelection() {
+    //Rock Paper Scissors Choses
+    let randomNum = Math.floor(Math.random() * (3) + 1) 
+    let selection = ''
+
+    if(randomNum === 1){
+        selection = 'rock'
+    }
+    if(randomNum === 2){
+        selection = 'paper'
+    }
+    if(randomNum === 3){
+        selection = 'scissors'
     }
     
-        //Scissors Win Condition
-    else if (autoSelection === 'scissors' && election === 'paper') {
-        alert('You Win')
-    }
-    
-    else{
-        alert('You Lose')
-    }
-    
+    return selection
+}
+
+function setScoreTable(playInformation) {
+    //Score Table
+    var scoreTable = document.getElementById('scoreTable');
+    var score = document.createElement('p');
+    score.textContent = `${playInformation[0]} , YOU(${playInformation[1].toUpperCase()}) vs BOT(${playInformation[2].toUpperCase()}))`;
+    scoreTable.appendChild(score);
 }
